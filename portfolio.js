ac016c7d68d3a01992a296b7a7180734989ba309
samuelsehnert <samuel.sehnert@gmail.com>
2022-10-07 13:35:19 -0700

    all tests work

--- portfolio.js
+++ portfolio.js
@@ -1,12 +1,42 @@
+class ShareSaleException extends Error {
+  constructor(message) {
+    super(message);
+    this.name = this.constructor.name;
+  }
+}
 
 const createStockPortfolio = () => {
   return {};
-}
+};
 
 const isPortfolioEmpty = (portfolio) => {
   return Object.keys(portfolio).length === 0;
-}
+};
 
+const countUniqueTickers = (portfolio) => {
+  return Object.keys(portfolio).length;
+};
+
+const addShares = (portfolio, ticker, numberOfShares) => {
+  !portfolio[ticker] ? portfolio[ticker] = numberOfShares : portfolio[ticker] += numberOfShares;
+  return portfolio;
+};
+
+const removeShares = (portfolio, ticker, numberOfShares) => {
+  if (!portfolio[ticker]) return portfolio;
+  portfolio[ticker] -= numberOfShares;
+  if (portfolio[ticker] < 0) throw new ShareSaleException("Attempting to sell more shares than owned");
+  if (portfolio[ticker] === 0) delete portfolio[ticker];
+  return portfolio;
+};
+
+const checkShares = (portfolio, ticker) => {
+  return !portfolio[ticker] ? 0 : portfolio[ticker];
+};
 
 exports.createStockPortfolio = createStockPortfolio;
 exports.isPortfolioEmpty = isPortfolioEmpty;
+exports.countUniqueTickers = countUniqueTickers;
+exports.addShares = addShares;
+exports.removeShares = removeShares;
+exports.checkShares = checkShares;
--- portfolio.test.js
+++ portfolio.test.js
@@ -1,17 +1,108 @@
 const portfolioFunctions = require('./portfolio.js');
 
-var emptyPortfolio;
+var startingPortfolio;
 
 beforeEach(() => {
-  emptyPortfolio = portfolioFunctions.createStockPortfolio();
+  startingPortfolio = portfolioFunctions.createStockPortfolio();
 });
 
-test('Init stock portfolio and check if empty', () => {
+test('Init stock portfolio and check if empty -- PASS', () => {
   const target = {};
-  expect(target).toEqual(emptyPortfolio);
+  expect(target).toEqual(startingPortfolio);
 });
 
-test('Check if portfolio is empty', () => {
-  const result = portfolioFunctions.isPortfolioEmpty(emptyPortfolio);
+test('Check if portfolio is empty -- PASS', () => {
+  const result = portfolioFunctions.isPortfolioEmpty(startingPortfolio);
   expect(result).toBeTruthy();
+});
+
+test('Check if portfolio is empty -- FAIL', () => {
+  startingPortfolio["TICKER"] = 1;
+  const result = portfolioFunctions.isPortfolioEmpty(startingPortfolio);
+  expect(result).toBeFalsy();
+});
+
+test('Check number of unique tickers of empty portfolio', () => {
+  const target = 0;
+  expect(target).toEqual(portfolioFunctions.countUniqueTickers(startingPortfolio));
+});
+
+test('Check number of unique tickers', () => {
+  startingPortfolio['FIRST'] = 8;
+  startingPortfolio['NEXT'] = 10;
+  startingPortfolio['THIRD'] = 15;
+  const target = 3;
+  expect(target).toEqual(portfolioFunctions.countUniqueTickers(startingPortfolio));
+});
+
+test('Add symbol and number of shares to empty portfolio', () => {
+  const target = {
+    "GME" : 10
+  };
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 10);
+  expect(target).toEqual(startingPortfolio);
+});
+
+test('Add multiple symbol and number of shares to empty portfolio', () => {
+  const target = {
+    "GME" : 10,
+    "TEST": 1000
+  };
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 10);
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "TEST", 1000);
+  expect(target).toEqual(startingPortfolio);
+});
+
+test('Add symbol and number of shares to non-empty portfolio', () => {
+  const target = {
+    "GME" : 15
+  };
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 10);
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 5);
+  expect(target).toEqual(startingPortfolio);
+});
+
+test('Subtract number of shares from empty portfolio', () => {
+  const target = {};
+  startingPortfolio = portfolioFunctions.removeShares(startingPortfolio, "GME", 5);
+  expect(target).toEqual(startingPortfolio);
+});
+
+test('Subtract number of shares from valid portfolio', () => {
+  const target = {
+    "GME" : 5
+  };
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 20);
+  startingPortfolio = portfolioFunctions.removeShares(startingPortfolio, "GME", 15);
+  expect(target).toEqual(startingPortfolio);
+});
+
+test('Subtract exact number of owned shares from valid portfolio', () => {
+  const target = {
+    'CSCO' : 100
+  };
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 20);
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "CSCO", 100);
+  startingPortfolio = portfolioFunctions.removeShares(startingPortfolio, "GME", 20);
+  expect(target).toEqual(startingPortfolio);
+});
+
+test('Subtract too many shares from valid portfolio -> ERROR', () => {
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 20);
+  expect(() => 
+    portfolioFunctions.removeShares(startingPortfolio, "GME", 100)
+  ).toThrowError("Attempting to sell more shares than owned");
+});
+
+test('Check number of shares for non-existing stock', () => {
+  const target = 0;
+  const result = portfolioFunctions.checkShares(startingPortfolio, "GME");
+  expect(target).toEqual(result);
+});
+
+test('Check number of shares for existing stock', () => {
+  const target = 20;
+  startingPortfolio = portfolioFunctions.addShares(startingPortfolio, "GME", 20);
+  const result = portfolioFunctions.checkShares(startingPortfolio, "GME");
+  expect(target).toEqual(result);
 });
